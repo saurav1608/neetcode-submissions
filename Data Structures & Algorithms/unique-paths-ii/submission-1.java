bb9c98a4a2c86fd72ae083f8d98a09dfde7e3b14
saurav1608 <saurav1608@gmail.com>
2026-06-30 23:32:44 +0530

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        return uniquePathsUsingArrays(obstacleGrid, obstacleGrid.length, obstacleGrid[0].length);
+    }
+
+    public int uniquePathsUsingArrays(int[][] obstacleGrid, int row, int col) {
+        int[] prevRow = new int[col];
+        //populate the previous row for first row
+        for (int c = 0; c < col ; c++) {
+            if (obstacleGrid[0][c] == 1) {
+                break;
+            }
+            prevRow[c] = 1;
+        }
+
+        //Start iterating for every other row
+        for (int r = 1; r < row; r++) {
+            prevRow[0] = (obstacleGrid[r][0] == 1) ? 0 : prevRow[0]; 
+            for (int c = 1; c < col; c++) {
+                if (obstacleGrid[r][c] == 1) {
+                    prevRow[c] = 0;
+                } else {
+                    prevRow[c] += prevRow[c-1];
+                }
+            }
+        }
+
+        return prevRow[col-1];
+    }
+
+    public int uniquePathsUsingMatrix(int[][] obstacleGrid, int row, int col) {
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[row-1][col-1] == 1) {
+            return 0;
+        }
+
+        int pathMatrix[][] = new int[row][col];
+
+        //populate the first row
+        for (int c = 0; c < col; c++) {
+            if (obstacleGrid[0][c] == 1) {
+                break;
+            }
+            pathMatrix[0][c] = 1;
+        }
+
+        //populate the first column
+        for (int r = 0; r < row; r++) {
+            if (obstacleGrid[r][0] == 1) {
+                break;
+            }
+            pathMatrix[r][0] = 1;
+        }
+
+        //start populating the grid
+        for (int r = 1; r < row; r++) {
+            for (int c = 1; c < col; c++) {
+                if (obstacleGrid[r][c] == 1) {
+                    pathMatrix[r][c] = 0;
+                } else {
+                    pathMatrix[r][c] = pathMatrix[r-1][c] + pathMatrix[r][c-1];
+                }
+            }
+        }
+        return pathMatrix[row-1][col-1];
+    }
+}
